ab0a1c76fabf8d8f3ba92ba12df6c57a35cdd54f
New Era Studios <46494643+New-Era-Studios@users.noreply.github.com>
2021-01-17 15:13:20 +0100

    Change in questions
    Different questions at startup

--- src/Start.java
+++ src/Start.java
@@ -7,8 +7,8 @@ public class Start
 		System.out.println("|     Welcome to L7-Sentry, what do you want to do today?      |");
 		System.out.println("|                                                              |");
 		System.out.println("|     1. Start the protection                                  |");
-		System.out.println("|     2. Change some settings                                  |");
-		System.out.println("|     3. Re-configurate everything                             |");
+		System.out.println("|     2. Where is my config file?                              |");
+		System.out.println("|     3. Generate a new config                                 |");
 		System.out.println("|     4. See protection statistics                             |");
 		System.out.println("|                                                              |");
 		System.out.println("|--------------------------------------------------------------|");
