4877d905f7e2d2527521bbb0b6d89ae4784fa667
New Era Studios <46494643+New-Era-Studios@users.noreply.github.com>
2021-01-17 15:07:35 +0100

    First commit
    This is the first commit, project has been initialized and started.

---
+++ src/Start.java
@@ -0,0 +1,18 @@
+public class Start
+{
+	public static void main(String[] args)
+	{
+		System.out.println("|--------------------------------------------------------------|");
+		System.out.println("|                                                              |");
+		System.out.println("|     Welcome to L7-Sentry, what do you want to do today?      |");
+		System.out.println("|                                                              |");
+		System.out.println("|     1. Start the protection                                  |");
+		System.out.println("|     2. Change some settings                                  |");
+		System.out.println("|     3. Re-configurate everything                             |");
+		System.out.println("|     4. See protection statistics                             |");
+		System.out.println("|                                                              |");
+		System.out.println("|--------------------------------------------------------------|");
+		System.out.println("");
+		System.out.print("# Answer: ");
+	}
+}
